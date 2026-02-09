554660aa451082aa00bb39a3f3832d9b170089c8
hyeeun0511 <bang48188@naver.com>
2026-02-09 18:10:32 +0900

    설정,request,model 데이터전달

---
+++ s0209_05/src/main/java/com/java/controller/FrontController.java
@@ -0,0 +1,27 @@
+package com.java.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.annotation.PostConstruct;
+
+@Controller
+public class FrontController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping("/doLogin")
+	public String doLogin() {
+		return "doLogin";
+	}
+
+}
